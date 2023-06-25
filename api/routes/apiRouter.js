696e7a4b5affdb14aebf07db4fa543b4500d6eb3
Paulo Toledo <ptoledo.bsices@gmail.com>
2023-06-25 18:54:25 -0300

    Configuring the knexfile

--- api/routes/apiRouter.js
+++ api/routes/apiRouter.js
@@ -1,21 +1,21 @@
 const express = require("express");
 let apiRouter = express.Router();
-const knex = require("knex")({
-  client: "pg",
-  debug: true,
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-  },
-});
-
-// apiRouter.use(express.json());
-// apiRouter.use(express.urlencoded({ extended: true }));
+const knex = require("knex")
+const knexfile = require('./knexfile');
+const knexProd = knex(knexfile.production);
+// ({
+//   client: "pg",
+//   debug: true,
+//   connection: {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: { rejectUnauthorized: false },
+//   },
+// });
 
 const endpoint = "/";
 
 apiRouter.get(endpoint + "carros", (req, res) => {
-  knex
+  knexProd
     .select("*")
     .from("carro")
     .then((carros) => res.status(200).json(carros))
@@ -28,7 +28,7 @@ apiRouter.get(endpoint + "carros", (req, res) => {
 
 apiRouter.get(endpoint + "carros/:id", (req, res) => {
   let id = req.params.id;
-  knex
+  knexProd
     .select("*")
     .from("carro")
     .where({ id })
@@ -41,7 +41,7 @@ apiRouter.get(endpoint + "carros/:id", (req, res) => {
 });
 
 apiRouter.post(endpoint + "carros", (req, res) => {
-  knex("carro")
+  knexProd("carro")
     .insert(req.body, ["id"])
     .then((carros) =>
       res
@@ -57,7 +57,7 @@ apiRouter.post(endpoint + "carros", (req, res) => {
 
 apiRouter.put(endpoint + "carros/:id", (req, res) => {
   let id = req.params.id;
-  knex("carro")
+  knexProd("carro")
     .where({ id })
     .update(req.body, ["id"])
     .then((carros) =>
@@ -74,7 +74,7 @@ apiRouter.put(endpoint + "carros/:id", (req, res) => {
 
 apiRouter.delete(endpoint + "carros/:id", (req, res) => {
   let id = req.params.id;
-  knex("carro")
+  knexProd("carro")
     .where({ id })
     .delete()
     .then(() =>
---
+++ knexfile.js
@@ -0,0 +1,34 @@
+// para criar novas tabelas: ./node_modules/.bin/knex migrate:make --env clinic [nome_da_tabela]
+
+require("dotenv").config();
+module.exports = {
+  develop: {
+    client: process.env.KNEX_PUBLIC_DATABASE_CLIENT,
+    connection: {
+      port: process.env.KNEX_PUBLIC_DATABASE_PORT,
+      host: process.env.KNEX_PUBLIC_DATABASE_HOST,
+      database: process.env.KNEX_PUBLIC_DATABASE_NAME,
+      user: process.env.KNEX_PUBLIC_DATABASE_USER,
+      password: process.env.KNEX_PUBLIC_DATABASE_PASSWD,
+    },
+    production: {
+      client: "pg",
+      debug: true,
+      connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      },
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "src/migrations",
+    },
+    seeds: {
+      directory: "src/seeds",
+    },
+  },
+};
